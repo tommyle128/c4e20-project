1b8a5b0d61aded487620cd9617388455ee521681
tommyle128 <minhtu.le128@gmail.com>
2018-09-22 21:50:05 +0700

    abc

--- webnovel.py
+++ webnovel.py
@@ -10,8 +10,8 @@ mlab.connect()
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/novel')
-def novel():
+@app.route('/<novel>')
+def novel(novel):
     all_novel = Novel.objects()
     return render_template('novel.html', all_novel = all_novel)
 
@@ -20,23 +20,80 @@ def chapter():
     all_chapter = Chapter.objects()
     return render_template('chapter.html', all_chapter = all_chapter)
 
-@app.route('/test/', methods=["GET", "POST"])
-def test():
-    if request.method == "GET":
-        return render_template ('test.html')
-    elif request.method == "POST":
-        test_form = request.form
-        name = test_form['name']
-        content = test_form['editor1']
-                
-        new_chapter = Chapter(
-            name=name,
-            content=content,
-        )
-        new_chapter.save()
 
-        return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
   app.run(debug=True)
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
