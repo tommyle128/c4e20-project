540e5405fbb9e8de6062fad7a116ea7d6f86f172
Katsuki <nguyenmson98@gmail.com>
2018-09-22 21:12:40 +0700

    .

--- up-novel.py
+++ up-novel.py
@@ -3,11 +3,6 @@ import mlab
 
 mlab.connect()
 
-new_chap = Chapter(
-    name = "Persona 5 chap 1",
-    content = "Joker",
-)
-new_chap.save()
 
 new_novel = Novel(
     name = 'Persona 5',
@@ -17,7 +12,33 @@ new_novel = Novel(
     introduce = 'abc',
     chapters = []
 )
-
 new_novel.save()    
-new_novel.update(push__chapters = new_chap)
+
+for i in range(2):
+    new_chap = Chapter(
+        name = 'Persona 5 chap ' + str(i),
+        content = "Joker",
+    )
+    new_chap.save()
+    
+    new_novel.update(push__chapters = new_chap)
+
+new_novel = Novel(
+    name = 'Persona 3',
+    author = 'Katsuki',
+    illu = 'game',
+    tag = ['action','mystery'],
+    introduce = 'abc',
+    chapters = []
+)
+new_novel.save()    
+
+for i in range(2):
+    new_chap = Chapter(
+        name = 'Persona 3 chap ' + str(i),
+        content = "Mona",
+    )
+    new_chap.save()
+    
+    new_novel.update(push__chapters = new_chap)
 
