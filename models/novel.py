55398daf137fcf2aecf913522d4def20571a6a14
Katsuki <nguyenmson98@gmail.com>
2018-09-29 19:23:42 +0700

    update

--- models/novel.py
+++ models/novel.py
@@ -1,4 +1,12 @@
 from mongoengine import *
+from wtforms import Form, StringField, SelectField
+ 
+class NovelSearchForm(Form):
+    choices = [('name', 'name'),
+               ('author', 'author'),
+               ('tag', 'tag')]
+    select = SelectField('Search for novel:', choices=choices)
+    search = StringField('')
 
 class Chapter(Document):
     name = StringField()
--- webnovel.py
+++ webnovel.py
@@ -1,7 +1,7 @@
 from flask import *
 import mlab
 from mongoengine import *
-from models.novel import Novel, Chapter, User
+from models.novel import Novel, Chapter, User, NovelSearchForm
 
 app = Flask(__name__)
 app.secret_key = 'a super super secret key'
@@ -21,25 +21,18 @@ def chapter():
     all_chapter = Chapter.objects()
     return render_template('chapter.html', all_chapter = all_chapter)
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template('login.html')
-    elif request.method == 'POST':
-        form = request.form 
+    elif request.method == "POST":
+        form = request.form
         username = form['username']
         password = form['password']
-
-@app.route('/signup', methods=["GET", "POST"])
-def signin():
-    if request.method == "GET":
-        return render_template('signup.html')
-    elif request.method == "POST":
         found_user = User.objects(
             username=username,
-            password=password
+            password=password,
         )
-
         if username == "":
             return "Hãy điền tên đăng nhập"
         else:
